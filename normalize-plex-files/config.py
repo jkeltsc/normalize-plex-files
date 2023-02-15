b666bdf2ec7d7afb5398d6c22c7b60deec9c1f54
Joachim Keltsch <joachim@keltsch.net>
2023-02-15 22:28:59 +0100

    fix some typos, print help if neither -m nor -T

--- normalize-plex-files/config.py
+++ normalize-plex-files/config.py
@@ -3,26 +3,53 @@ import sys
 import os
 from types import SimpleNamespace
 
+
 def getconfig() -> SimpleNamespace:
     """Gets various config variables from the environment or from the commandline."""
+
+    def usage():
+        print(f"""usage: normalize-plex-files {{-m [-T] | -T [-m]}} [--armed] [-d] [-D database] \\
+                [-b moviedir] [-l libraryname] [-s #subdirs] [-o] \\
+                [-B seriesdir] [-L libraryname] [-S #subdirs] [-O]
+
+    Arguments and Environment Variables:
+            --armed                                     run armed, default: unarmed (no files touched)
+        -m | --movies                                    process movie files, default: do not process movies
+        -b | --moviesbase dir       PLEX_MOVIESBASE      movie files directory, env/default: {config.moviesbase}
+        -l | --movieslibrary name   PLEX_MOVIESLIBRARY   movies library name, eenv/default: {config.movieslibrary}
+        -s | --moviessubdirs #      PLEX_MOVIESSUBDIRS   levels of subdirs to retain, env/default: {config.moviessubdirs}
+        -o | --ownmoviefolder       PLEX_OWNMOVIEFOLDER  pack each movie in its own movie folder, env/default: {config.ownmoviefolder}
+        -T | --tvseries                                  process tv series, default: do not process tv series
+        -B | --seriesbase dir       PLEX_SERIESBASE      series files directory, env/default: {config.seriesbase}
+        -L | --serieslibrary name   PLEX_SERIESLIBRARY   series library name, env/default: {config.serieslibrary}
+        -S | --serieessubdirs #     PLEX_SERIESSUBDIRS   levels of subdirs to retain, env/default: {config.moviessubdirs}
+        -O | --ownseasonfolder      PLEX_OWNSEASONFOLDER pack each season in its own season folder, env/default: {config.ownseasonfolder}
+        -d | --debug                                     turn on debug messages, default: no debug messages
+        -D | --database file        PLEX_DATABASE        database file, env/default: {config.database}
+    """, file=sys.stderr)
+        sys.exit(2)
 
     config = SimpleNamespace()
 
     config.armed = False
     config.debug = False
-    config.database = os.environ.get('PLEX_DATABASE', '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db')
+    config.database = os.environ.get(
+        'PLEX_DATABASE', '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db')
 
     config.movies = False
     config.moviesbase = os.environ.get('PLEX_MOVIESBASE', '/data/plex/Filme/')
     config.movieslibrary = os.environ.get('PLEX_MOVIESLIBRARY', "Filme")
-    config.moviessubdirs = abs(int(os.environ.get('PLEX_MOVIESSUBDIRS',1)))
-    config.ownmoviefolder = os.environ.get('PLEX_OWNMOVIEFOLDER', False) == True
+    config.moviessubdirs = abs(int(os.environ.get('PLEX_MOVIESSUBDIRS', 1)))
+    config.ownmoviefolder = os.environ.get(
+        'PLEX_OWNMOVIEFOLDER', False) == True
 
     config.series = False
-    config.seriesbase = os.environ.get('PLEX_SERIEESSBASE', '/data/plex/Serien/')
+    config.seriesbase = os.environ.get(
+        'PLEX_SERIESBASE', '/data/plex/Serien/')
     config.serieslibrary = os.environ.get('PLEX_SERIESLIBRARY', "Serien")
     config.seriessubdirs = abs(int(os.environ.get('PLEX_SERIESSUBDIRS', 1)))
-    config.ownseasonfolder = os.environ.get('PLEX_OWNSERIESFOLDER', False) == True
+    config.ownseasonfolder = os.environ.get(
+        'PLEX_OWNSEASONFOLDER', False) == True
 
     try:
         opts, args = getopt.getopt(
@@ -42,27 +69,7 @@ def getconfig() -> SimpleNamespace:
                 "database="
             ])
     except getopt.GetoptError as err:
-        print(f"""
-usage: normalize [--armed] [-d] [-D {{database}}] \\
-                    [-m] [-b {{moviedir}}] [-l {{libraryname}}] [-s {{#subdirs}}] [-o] \\
-                    [-t] [-B {{seriesdir}}] [-L {{libraryname}}] [-S {{#subdirs}}] [-O]
-
-Arguments and Environment Variables:
-         --armed                                     run armed, default: unarmed (no files touched)
-    -m | --movies                                    process movie files, default: do not process movies
-    -b | --moviesbase {{dir}}     PLEX_MOVIESBASE      movie files directory, env/default: {config.moviesbase}
-    -l | --movieslibrary {{name}} PLEX_MOVIESLIBRARY   movies library name, eenv/default: {config.movieslibrary}
-    -s | --moviessubdirs {{#}}    PLEX_MOVIESSUBDIRS   levels of subdirs to retain, env/default: {config.moviessubdirs}
-    -o | --ownmoviefolder       PLEX_OWNMOVIEFOLDER  pack each movie in its own movie folder, env/default: {config.ownmoviefolder}
-    -T | --tvseries                                  process tv series, default: do not process tv series
-    -B | --seriesbase {{dir}}     PLEX_SERIEESSBASE    series files directory, env/default: {config.seriesbase}
-    -L | --serieslibrary {{name}} PLEX_SERIESLIBRARY   series library name, env/default: {config.serieslibrary}
-    -S | --serieessubdirs {{#}}   PLEX_SERIESSUBDIRS   levels of subdirs to retain, env/default: {config.moviessubdirs}
-    -O | --ownseasonfolder      PLEX_OWNSERIESFOLDER pack each season in its own season folder, env/default: {config.ownseasonfolder}
-    -d | --debug                                     turn on debug messages, default: no debug messages
-    -D | --database {{file}}      PLEX_DATABASE        database file, env/default: {config.database}
-""", file=sys.stderr)
-        sys.exit(2)
+        usage()
 
     for o, a in opts:
         if o == "--armed":
@@ -90,15 +97,12 @@ Arguments and Environment Variables:
         if o in ("-O", "--ownseasonfolder"):
             config.ownseasonfolder = True
 
-
         if o in ("-d", "--debug"):
             config.debug = True
         if o in ("-D", "--database"):
             config.database = a
 
-
     if not config.movies and not config.series:
-        print("Doing nothing. Neither -m nor -T specified.", file=sys.stderr)
-        sys.exit(1)
+        usage()
 
     return config
